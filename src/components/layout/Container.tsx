f5d14bd202ef98ec84579dadb003ff82898c2ece
minha2523 <ashikurrahmanminha2007@gmail.com>
2026-01-13 20:34:36 +0600

    Sync from AR GPT - 2026-01-13T14:33:24.551Z

---
+++ src/components/layout/Container.tsx
@@ -0,0 +1,30 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+function cn(...inputs: (string | undefined | null | boolean)[]) {
+  return twMerge(clsx(inputs));
+}
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  size?: 'sm' | 'md' | 'lg' | 'xl' | 'full';
+  children: React.ReactNode;
+}
+
+export const Container = ({ size = 'lg', className, children, ...props }: ContainerProps) => {
+  const sizes = {
+    sm: 'max-w-3xl',
+    md: 'max-w-5xl',
+    lg: 'max-w-7xl',
+    xl: 'max-w-[1400px]',
+    full: 'max-w-full',
+  };
+
+  return (
+    <div
+      className={cn('mx-auto px-4 sm:px-6 lg:px-8 w-full', sizes[size], className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
