098c06f6707a9536c85e9b42322861362b8a3f20
minha2523 <ashikurrahmanminha2007@gmail.com>
2026-01-13 20:33:32 +0600

    Sync from AR GPT - 2026-01-13T14:33:24.551Z

---
+++ src/components/ui/Skeleton.tsx
@@ -0,0 +1,15 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+function cn(...inputs: (string | undefined | null | boolean)[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const Skeleton = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
+  return (
+    <div
+      className={cn('animate-pulse rounded-md bg-muted', className)}
+      {...props}
+    />
+  );
+};
