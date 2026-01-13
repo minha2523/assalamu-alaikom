1eb818780f8ae78ef2917829fdf8f4a2d48a01dc
minha2523 <ashikurrahmanminha2007@gmail.com>
2026-01-13 20:35:17 +0600

    Sync from AR GPT - 2026-01-13T14:33:24.551Z

---
+++ src/components/ui/Label.tsx
@@ -0,0 +1,24 @@
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+function cn(...inputs: (string | undefined | null | boolean)[]) {
+  return twMerge(clsx(inputs));
+}
+
+interface LabelProps extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  children: React.ReactNode;
+}
+
+export const Label = ({ className, children, ...props }: LabelProps) => {
+  return (
+    <label
+      className={cn(
+        'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </label>
+  );
+};
