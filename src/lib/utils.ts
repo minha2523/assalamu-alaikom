db15d426fe1a13a721bc3e29657de37e00a8b1ca
minha2523 <ashikurrahmanminha2007@gmail.com>
2026-01-13 20:33:45 +0600

    Sync from AR GPT - 2026-01-13T14:33:24.551Z

---
+++ src/lib/utils.ts
@@ -0,0 +1,38 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number, currency = 'USD') {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+  }).format(price);
+}
+
+export function formatDate(date: Date | string, options?: Intl.DateTimeFormatOptions) {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    ...options,
+  }).format(new Date(date));
+}
+
+export function truncate(str: string, length: number) {
+  if (str.length <= length) return str;
+  return str.slice(0, length) + '...';
+}
+
+export function slugify(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/[^a-z0-9]+/g, '-')
+    .replace(/(^-|-$)/g, '');
+}
+
+export function generateId() {
+  return Math.random().toString(36).substring(2, 9);
+}
